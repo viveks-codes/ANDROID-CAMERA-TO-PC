86b2b3025ef0d995738a76397f477a0a88589587
vivolscute <55041874+vivolscute@users.noreply.github.com>
2020-08-29 11:37:56 +0530

    Add files via upload

---
+++ cam.py
@@ -0,0 +1,15 @@
+#follow Vivek_Codes for more intresting Tech memes and Projects
+import requests 
+import cv2 
+import numpy as np 
+
+url = "your URL HERE!!!" + /shot.jpg" #
+
+while True:
+    response = requests.get(url) #send GET request to URL
+    arr = np.array(bytearray(response.content),dtype=np.uint8) #converting response to numpy array
+    img = cv2.imdecode(arr,-1)#decoding numpy array
+    
+    cv2.imshow("Vivek_Codes", img)# showing image
+    if cv2.waitKey(1) == 27: #if usr press esc key thn break the while loop
+        break
